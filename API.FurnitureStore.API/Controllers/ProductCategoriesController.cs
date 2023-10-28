c32d2e6060c438ee07f1d5b5963f0d36aaf64730
agustinlopez23 <agustinlopez223@gmail.com>
2023-10-28 01:18:54 -0300

    Add project files.

---
+++ API.FurnitureStore.API/Controllers/ProductCategoriesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using API.FurnitureStore.Data;
+using API.FurnitureStore.Shared;
+using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.FurnitureStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductCategoriesController : ControllerBase
+    {
+        private readonly ApiFurnitureStoreContext _context;
+        public ProductCategoriesController(ApiFurnitureStoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<ProductCategory>> Get()
+        {
+             return await _context.ProductCategories.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDetails(int id)
+        {
+            var productCategory = await _context.ProductCategories.FirstOrDefaultAsync(p => p.Id == id);
+            if (productCategory == null) return NotFound();
+            return Ok(productCategory);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(ProductCategory category)
+        {
+            await _context.ProductCategories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("Post",category.Id, category);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Put(ProductCategory category)
+        {
+            _context.ProductCategories.Update(category);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpDelete]
+        public async Task<IActionResult>Delete(ProductCategory category)
+        {
+            if(category == null) return NotFound();
+            _context.ProductCategories.Remove(category);
+            await _context.SaveChangesAsync();
+            return NoContent();
+
+        }
+    }
+}
---
+++ API.FurnitureStore.API/Controllers/ProductsController.cs
@@ -0,0 +1,59 @@
+﻿using API.FurnitureStore.Data;
+using API.FurnitureStore.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.FurnitureStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApiFurnitureStoreContext _context;
+        public ProductsController(ApiFurnitureStoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _context.Products.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDetails(int id)
+        {
+            var product = await _context.Clients.FirstOrDefaultAsync(c => c.Id == id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpGet("GetByCategory/{productCategoryId}")]
+        public async Task<IEnumerable<Product>>GetByCategory(int productCategoryId)
+        {
+            return await _context.Products.Where(p=>p.ProductCategoryId==productCategoryId).ToListAsync();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("Post", product.Id, product);
+
+        }
+        [HttpPut]
+        public async Task<IActionResult>Put(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return Ok(product);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Product product)
+        {
+            if(product==null) return NotFound();
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
